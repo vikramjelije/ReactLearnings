fc7e2344daddb642404b49d3bd4081f39a8336ab
jelije vikram <jelijevikram@jelijes-MacBook-Air.local>
2024-01-27 16:25:20 +0530

    completed section 4

--- animal/src/AnimalShow.js
+++ animal/src/AnimalShow.js
@@ -1,7 +1,34 @@
-function AnimalShow() {
+import { useState  } from 'react';
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+import './AnimalShow.css';
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}
+
+function AnimalShow({ type }) {
+
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
     return (
-        <div>
-            Show animals here!
+        <div onClick={handleClick} className='animal-show'>
+            <img alt="animal" src={svgMap[type]} width="400px" height="400px" className='animal'/>
+            <img alt = "heart" src={heart} style={{ width : 10 + 10 * clicks + 'px' }} className='heart'/>
         </div>
     );
 }
--- animal/src/App.js
+++ animal/src/App.js
@@ -1,17 +1,27 @@
 import AnimalShow from "./AnimalShow";
-import { useState } from 'react';
+import { useState } from "react";
+import './App.css';
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
-    const handleClick = () => {
-        setCount(count + 1);
-    };
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
 
-    return (<div>
-        <button onClick={handleClick}>Add animal</button>
-        <p>Count:{count}</p>
-    </div>);
+  const renderedAnimals = animals.map(( animal, index) => <AnimalShow type={animal} key={index}/>)
+
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
+  );
 }
 
 export default App;
